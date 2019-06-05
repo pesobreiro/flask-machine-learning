63dcc5931a06f0b1ab0ea02bbe72da2c5b969b6a
pesobreiro@gmail.com <pesobreiro@gmail.com>
2019-06-05 15:42:42 +0100

    update

--- flask-predictor/server.py
+++ flask-predictor/server.py
@@ -1,10 +1,9 @@
 # Create API of ML model using flask
-
+import requests
 '''
 This code takes the JSON data while POST request an performs the prediction using loaded model and returns
 the results in JSON format.
 '''
-
 # Import libraries
 import numpy as np
 from flask import Flask, request, jsonify, render_template
@@ -14,8 +13,6 @@ import textwrap
 
 app = Flask(__name__)
 
-# Load the model
-model = pickle.load(open('model.pkl','rb'))
 @app.route('/')
 @app.route('/home')
 def home():
@@ -30,24 +27,23 @@ def contact():
     return render_template('contact.html', title='Home Page', year=datetime.now().year)
 
 
-@app.route("/teste")
+@app.route('/teste')
 def teste():
-    return "12"
-    """
-    <html>
-    <header><title>Teste</title></header>
-    <body>
-        teste
-    </body>
-    <br>
-    </html>
-    """
+    url = 'http://localhost:5000/api'
+
+    # Change the value of experience that you want to test
+    r = requests.post(url, json={'exp': 1.8, })
+    return r.json()
 
 
 @app.route('/api',methods=['POST'])
 def predict():
     # Get the data from the POST request.
     data = request.get_json(force=True)
+
+
+    # Load the model
+    model = pickle.load(open('model.pkl', 'rb'))
 
     # Make prediction using model loaded from disk as per the data.
     prediction = model.predict([[np.array(data['exp'])]])
